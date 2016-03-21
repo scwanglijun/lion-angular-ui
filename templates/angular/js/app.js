2dab803e0d46aaa1fa7542eb3d1a42640be55d25
wash <1721445902@qq.com>
2016-03-21 16:24:39 +0800

    angular-ui页面结构搭建

---
+++ templates/angular/js/app.js
@@ -0,0 +1,88 @@
+/***
+Metronic AngularJS App Main Script
+***/
+
+/* Metronic App */
+var MetronicApp = angular.module("MetronicApp", [
+    "ui.router", 
+    "ui.bootstrap", 
+    "oc.lazyLoad",  
+    "ngSanitize"
+]); 
+
+/* Setup App Main Controller */
+MetronicApp.controller('AppController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    $scope.$on('$viewContentLoaded', function() {
+        Metronic.initComponents(); // init core components
+        //Layout.init(); //  Init entire layout(header, footer, sidebar, etc) on page load if the partials included in server side instead of loading with ng-include directive
+    });
+}]);
+
+
+/* Setup global settings */
+MetronicApp.factory('settings', ['$rootScope', function($rootScope) {
+    // supported languages
+    var settings = {
+        layout: {
+            pageSidebarClosed: false, // sidebar state
+            pageAutoScrollOnLoad: 1000 // auto scroll to top on page load
+        },
+        layoutImgPath: Metronic.getAssetsPath() + 'admin/layout/img/',
+        layoutCssPath: Metronic.getAssetsPath() + 'admin/layout/css/'
+    };
+
+    $rootScope.settings = settings;
+
+    return settings;
+}]);
+
+
+/* Configure ocLazyLoader(refer: https://github.com/ocombe/ocLazyLoad) */
+MetronicApp.config(['$ocLazyLoadProvider', function($ocLazyLoadProvider) {
+    $ocLazyLoadProvider.config({
+        cssFilesInsertBefore: 'ng_load_plugins_before' // load the above css files before a LINK element with this ID. Dynamic CSS files must be loaded between core and theme css files
+    });
+}]);
+
+
+/* Setup Layout Part - Header */
+MetronicApp.controller('HeaderController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initHeader(); // init header
+    });
+}]);
+
+MetronicApp.controller('SidebarController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initSidebar(); // init sidebar
+    });
+}]);
+
+MetronicApp.controller('PageHeadController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {        
+        Demo.init(); // init theme panel
+    });
+}]);
+
+/* Setup Layout Part - Footer */
+MetronicApp.controller('FooterController', ['$scope', function($scope) {
+    $scope.$on('$includeContentLoaded', function() {
+        Layout.initFooter(); // init footer
+    });
+}]);
+
+
+//配置路由
+MetronicApp.config(['$stateProvider', '$urlRouterProvider',function($stateProvider, $urlRouterProvider){
+   
+   $urlRouterProvider.otherwise('/dashboard.html');
+
+   $stateProvider
+        // Dashboard
+        .state('dashboard', {
+            url: "/dashboard.html",
+            templateUrl: "views/dashboard.html",            
+            data: {pageTitle: 'Dashboard', pageSubTitle: 'statistics & reports'}
+        });
+}]);
+
